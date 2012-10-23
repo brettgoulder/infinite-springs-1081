57b398c5f94ab7a10c6883712d3843ecab6f7a72
Brett Goulder <brett.goulder@gmail.com>
2012-10-24 00:13:52 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Woot::Application.config.secret_token = '87c26db4f29d3c3f2b17a267bd44aa13cf2081c170e5f5972f4d222bb0e2f90955dbb1d3c40594358929bcb3d1939862ff70288857468cc328c1a23ec4739c33'
